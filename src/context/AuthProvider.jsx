bf0dfe77abbe64fdc0d814d894b6cca5da8883a7
yvzklc <kilciyavuz34@gmail.com>
2023-12-25 20:25:49 +0300

    first commit

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,25 @@
+//AuthContext
+
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
+
+  const login = (info) => {
+    setUser(info);
+    navigate("/dashboard");
+  };
+const logout = () => {
+  setUser(null)
+}
+  return (
+    <AuthContext.Provider value={{ user, login,logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
